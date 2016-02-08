7239bbbe9eac63b65988e8f92cced280b774ca73
Katherine Moloney <kmoloney@mail.ccsf.edu>
2016-02-07 19:47:29 -0800

    add IBAction onLogin for Button 'Login with Twitter' in ViewController

--- twitter-oauth-demo/ViewController.swift
+++ twitter-oauth-demo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import BDBOAuth1Manager
 
 class ViewController: UIViewController {
 
@@ -18,6 +19,24 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func onLogin(sender: AnyObject) {
+        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
+        
+        TwitterClient.sharedInstance.fetchRequestTokenWithPath(
+                "oauth/request_token",
+                method: "GET",
+                callbackURL: NSURL(string: "cptwitter://oauth"),
+                scope: nil,
+                success: {
+                        (requestToken: BDBOAuth1Credential!) -> Void in
+                            print("Got the request token")
+            }) {
+                (error: NSError!) -> Void in
+                    print("Failed to get request token")
+            }
     }
 
 
