1a5323ad3195e219a8915ed10eb4c23820b7772d
Katherine Moloney <kmoloney@mail.ccsf.edu>
2016-02-07 19:00:24 -0800

    create TwitterClient.swift + add BDBOAuth1Manager import

---
+++ twitter-oauth-demo/TwitterClient.swift
@@ -0,0 +1,14 @@
+//
+//  TwitterClient.swift
+//  twitter-oauth-demo
+//
+//  Created by mny on 2/7/16.
+//  Copyright © 2016 ccsf. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class TwitterClient: BDBOAuth1SessionManager {
+
+}
