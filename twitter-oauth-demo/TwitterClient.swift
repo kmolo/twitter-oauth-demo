5ac4b2645765a92b298c8aeb071e184f9362f541
Katherine Moloney <kmoloney@mail.ccsf.edu>
2016-02-20 01:51:37 -0800

    refactor login code - move out of ViewController to TwitterClient
    - move fetchRequestTokenWithPath function from ViewController.swift to
    TwitterClient.swift

--- twitter-oauth-demo/TwitterClient.swift
+++ twitter-oauth-demo/TwitterClient.swift
@@ -15,6 +15,8 @@ let twitterConsumerSecret = ""
 
 class TwitterClient: BDBOAuth1SessionManager {
     
+    var loginCompletion: ( (user: User?, error: NSError?) -> () )?
+    
     class var sharedInstance: TwitterClient {
         struct Static {
             static let instance = TwitterClient(
@@ -24,6 +26,32 @@ class TwitterClient: BDBOAuth1SessionManager {
         }
         
         return Static.instance
+    }
+    
+    func loginWithCompletion(completion: (user: User?, error: NSError?) -> ()) {
+        
+        loginCompletion = completion
+        
+        //Fetch request token + redirect to login page
+        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
+        TwitterClient.sharedInstance.fetchRequestTokenWithPath(
+            "oauth/request_token",
+            method: "GET",
+            callbackURL: NSURL(string: "cputwitterdemo://oauth"),
+            scope: nil,
+            success: {
+                (requestToken: BDBOAuth1Credential!) -> Void in
+                print("Got the request token")
+                
+                let authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")
+                
+                UIApplication.sharedApplication().openURL(authURL!)
+                
+            }) {
+                (error: NSError!) -> Void in
+                print("Failed to get request token")
+        }
+        
     }
 
 }
