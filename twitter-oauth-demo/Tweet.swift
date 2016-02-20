30647ab9a5359767062efc60026766cbc973f0ee
Katherine Moloney <kmoloney@mail.ccsf.edu>
2016-02-20 00:25:58 -0800

    create function tweetsWithArray to parse an array of Tweets

--- twitter-oauth-demo/Tweet.swift
+++ twitter-oauth-demo/Tweet.swift
@@ -15,14 +15,24 @@ class Tweet: NSObject {
     var createdAt: NSDate?
     
     init(dictionary: NSDictionary) {
-        user = User(dictionary: dictionary["user"] as! NSDictionary);
-        text = dictionary["text"] as? String;
-        createdAtString = dictionary["created_at"] as? String;
+        user = User(dictionary: dictionary["user"] as! NSDictionary)
+        text = dictionary["text"] as? String
+        createdAtString = dictionary["created_at"] as? String
         
-        let formatter = NSDateFormatter();
-        formatter.dateFormat = "EEE MMM d HH:mm:ss Z y";
-        createdAt = formatter.dateFromString(createdAtString!);
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
+        createdAt = formatter.dateFromString(createdAtString!)
         
+    }
+    
+    class func tweetsWithArray(array: [NSDictionary]) -> [Tweet] {
+        var tweets = [Tweet]()
+        
+        for dictionary in array {
+            tweets.append(Tweet(dictionary: dictionary))
+        }
+        
+        return tweets
     }
     
 }
